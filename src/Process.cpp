11e033457cd7c1ffddd8cce0253098900ca15c09
Erel Nahum <erelnahum2003@gmail.com>
2025-06-18 14:13:41 -0700

    Add find_memory_regions

--- src/Process.cpp
+++ src/Process.cpp
@@ -1,10 +1,12 @@
 #include "Process.h"
 
+#include <iostream>
+
 #include "Exceptions.h"
 #include "WinApi.h"
 
 Process::Process(HandleGuard&& process_handle)
-	: m_process_handle(std::move(process_handle)) {}
+	: m_process_handle{std::move(process_handle)} {}
 
 std::vector<std::byte> Process::read(
 	const void* const address,
@@ -17,4 +19,26 @@ std::vector<std::byte> Process::read(
 void Process::write(void* const address, std::span<const std::byte> data) const
 {
 	windows::write_process_memory(m_process_handle.get(), address, data);
+}
+
+std::vector<MemoryRegion> Process::find_memory_regions() const
+{
+	const auto system_info = windows::get_system_info();
+	auto address = reinterpret_cast<std::uintptr_t>(system_info.lpMinimumApplicationAddress);
+	const auto end_address = reinterpret_cast<std::uintptr_t>(system_info.lpMaximumApplicationAddress);
+
+	std::vector<MemoryRegion> memory_regions{};
+	while (address < end_address) {
+		try {
+			const auto region_information = windows::virtual_query_ex(
+				m_process_handle.get(),
+				reinterpret_cast<void*>(address)
+			);
+			memory_regions.emplace_back(region_information);
+			address += region_information.RegionSize;
+		} catch (const WindowsException& exception) {
+			address += system_info.dwPageSize;
+		}
+	}
+	return memory_regions;
 }
--- src/Process.h
+++ src/Process.h
@@ -5,12 +5,14 @@
 #include <cstddef>
 
 #include "Guards.h"
+#include "MemoryRegion.h"
 
 class Process {
 public:
 	explicit Process(HandleGuard&& process_handle);
 	std::vector<std::byte> read(const void* address, size_t size) const;
 	void write(void* address, std::span<const std::byte> data) const;
+	std::vector<MemoryRegion> find_memory_regions() const;
 private:
 	HandleGuard m_process_handle;
 };
