2819fea4abb398cbf8818ab3fd825f78b3fe2796
Erel Nahum <erelnahum2003@gmail.com>
2025-06-19 12:09:05 -0700

    Remove unused import

--- src/Exceptions.cpp
+++ src/Exceptions.cpp
@@ -1,6 +1,5 @@
 #include "Exceptions.h"
 
-#include <iostream>
 
 WindowsException::WindowsException(const uint32_t error_code)
     : std::system_error{std::error_code(error_code, std::system_category())} {}
